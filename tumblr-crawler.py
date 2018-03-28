a5b532d4d9a6089de12331c4aa589a46080b8433
Joanna Janaszek <janaszek@onet.pl>
2018-03-28 21:04:04 +0200

    refactoring

--- tumblr-crawler.py
+++ tumblr-crawler.py
@@ -1,25 +1,22 @@
 # -*- coding: utf-8 -*-
-
-# TO DO:
-#     pobrać posty z 100 blogów
-#     json order not working
  
 import sys
 import time
 import argparse
 import urllib.parse
 from urllib.parse import urlparse
+from langdetect import detect
 import json
 import requests
 import html
-from collections import OrderedDict
-import hashlib 
+import hashlib
+import socket
  
 class TumblrPost:
 
-    def __init__(self, post_iid, post_url, post_date, post_title, post_body):
+    def __init__(self, post_iid, post_id, post_url, post_date, post_title, post_body):
         self.iid = post_iid
-        self.id = post_iid + "-1"
+        self.id = post_id
         self.lname = "tumblr.com"
         self.post_url = post_url
         self.post_date = post_date
@@ -32,13 +29,6 @@ class TumblrPost:
         return text
 
 def save_as_json(post):
-    post_lname = post.lname
-    post_url = post.post_url
-    post_iid = post.iid
-    post_id = post.id
-    post_title = post.post_title
-    post_content = post.post_body
-    post_date = post.post_date
     return ({"lname": post.lname,
                        "url": post.post_url,
                        "iid": post.iid,
@@ -47,18 +37,6 @@ def save_as_json(post):
                        "content": post.post_body,
                        "duration_start": post.post_date,
                        "duration_end": post.post_date})
-    # post_dict = OrderedDict(
-    #     lname= post_lname,
-    #     url= post_url,
-    #     iid= post_iid,
-    #     id= post_id,
-    #     title= post_title,
-    #     content= post_content,
-    #     duration_start= post_date,
-    #     duration_end= post_date
-    # )
-    # #return json.dumps(post_dict, indent=4, ensure_ascii=False)
-    # return post_dict
 
 def remove_html_tags(text):
     import re
@@ -73,14 +51,22 @@ def get_json_page(url, start=0, num=50):
     args['start'] = start
     args['num'] = num
  
-    # Fetch the page
-    r = requests.get(url, params=args)
- 
-    # Strip the jsonp garbage
-    text = r.text.strip()[22:-1]
- 
-    # Return the result as a dict
-    return json.loads(text)
+    try:
+        # Fetch the page
+        r = requests.get(url, params=args)
+     
+        # Strip the jsonp garbage
+        text = r.text.strip()[22:-1]
+     
+        # Return the result as a dict
+        return json.loads(text)
+    except requests.exceptions.ConnectionError:
+        r.status_code = 'Connection refused'
+        print('Connection refused. Waiting...')
+        time.sleep(50)
+        print('Trying again...')
+    except UnicodeError:
+        print('UnicodeError: label empty or too long. Skipping...')
  
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
     enc = file.encoding
@@ -115,84 +101,112 @@ def get_and_save_post(json_page, all_posts):
             title = json_page['regular-title'] if "regular-title" in json_page else ""
             found = True
  
-        if found:
-            title = create_title(body) if title == '' else title
-
-            # Create iid for solar
-            post_iid = hashlib.md5(json_page['url'].encode('utf-8')).hexdigest()
-            # Date format for solar
-            date = json_page['date-gmt'].split(' ')
-            post_date = '{}T{}Z'.format(date[0], date[1])
+        if found:            
 
             body = html.unescape(remove_html_tags(u"{}".format(body)))
-            title = html.unescape(remove_html_tags(u"{}".format(title)))
+            lang = detect(body)
+            if lang == 'pl':
 
-            p = TumblrPost(post_iid, json_page['url'], post_date, title, body)
-            all_posts.append(p)
- 
-def main():
-    #global parser
-    #parser = argparse.ArgumentParser(prog='tumblr-scraper',
-            #description='Get Tumblr blog posts')
- 
-    # Set up our command-line arguments
-    #parser.add_argument('blog_name') 
-    #args = parser.parse_args() 
-    #blog_name = args.blog_name
+                title = create_title(body) if title == '' or title == None else title
 
-    blog_name = 'czytanki'
+                # Create iid, id for solr
+                post_iid = int(hashlib.md5(json_page['url'].encode('utf-8')).hexdigest(), 16)
+                post_id = str(post_iid) + '-1'
+
+                # Date format for solr
+                date = json_page['date-gmt'].split(' ')
+                post_date = '{}T{}Z'.format(date[0], date[1])
+
+                title = html.unescape(remove_html_tags(u"{}".format(title)))
+
+                p = TumblrPost(post_iid, post_id, json_page['url'], post_date, title, body)
+                all_posts.append(p)
+                # print('Got %s language. Saving.' % lang)
+                return True
+            else:
+                print('Got %s language. Not saving.' % lang)
+                print('Searching for other blogs...')
+                return False
+
+links_to_blogs = set()
+count = 0
+choosen_ln = 'pl'
+
+def get_posts_from_blogs(blog_name, count_blogs):
+
+    print('Getting posts from ' + blog_name + '...')
+
     blog_url = get_blog_url(blog_name) 
     json_page = get_json_page(blog_url, 0, 0)
-    total_count = json_page['posts-total'] 
+    total_count = json_page['posts-total']
     uprint('Total posts: %s' % (total_count,))
  
     start_time = time.time()
 
     # Get posts
     all_posts = []
+    global links_to_blogs
     start = 0
     per_page = 50
-    while start < total_count:
+    ln_ok = True
+    while start < total_count and total_count < 10000:
         json_page = get_json_page(blog_url, start)
- 
-        for post in json_page['posts']:
-            get_and_save_post(post, all_posts)
- 
-        # Increment and grab the next batch of posts
-        start += per_page
+        
+        if 'posts' in json_page:
+            for post in json_page['posts']:
+                if ln_ok:
+                    ln_ok = get_and_save_post(post, all_posts)
+
+                if 'reblogged-root-name' in post:
+                    link_to_blog = post['reblogged-root-name']
+                    if not 'deactivated' in link_to_blog:
+                        try:
+                            r = requests.get(get_blog_url(link_to_blog))
+                            if  r.ok:
+                                links_to_blogs.add(link_to_blog)
+                        except requests.exceptions.ConnectionError:
+                            r.status_code = 'Connection refused'
+                            print('Connection refused')
+                            time.sleep(50)
+                        except UnicodeError:
+                            print('UnicodeError: label empty or too long')
+     
+            # Increment and grab the next batch of posts
+            start += per_page
 
     # Save to file
-    with open('posts.json', 'w', encoding='utf-8') as f:
+    file_name = 'posts%s.json' % count_blogs
+    with open(file_name, 'w', encoding='utf-8') as f:
         posts = {}
         posts['posts'] = []
         for post in all_posts:
             posts['posts'].append(save_as_json(post))
         f.write(json.dumps(posts, indent=4, ensure_ascii=False))
+
+    global count
+    if len(all_posts) != 0:
+        count += 1
  
     minutes, seconds = divmod(time.time() - start_time, 60)
-    uprint('Got {count} posts in {m:02d}m and {s:02d}s'.format(\
-            count=total_count, m=int(round(minutes)), s=int(round(seconds))))
+    uprint('Got {count} text posts in {m:02d}m and {s:02d}s'.format(\
+            count=len(all_posts), m=int(round(minutes)), s=int(round(seconds))))
+ 
+def main():
+    global parser
+    parser = argparse.ArgumentParser(prog='tumblr-scraper',
+            description='Get Tumblr blog posts')
+ 
+    # Set up command-line arguments
+    parser.add_argument('blog_name') 
+    args = parser.parse_args() 
+    blog_name = args.blog_name
 
-    add_to_solar()
+    global links_to_blogs
+    links_to_blogs.add(blog_name)
 
-def add_to_solar():
-    # server = 
-    # core = 'isi'
-    # url = 'http://%s:8983/solr/%s/update/json/docs?commit=true' % (server, core)
-
-    posts = []
-    with open('posts.json', encoding='utf-8') as get_post:
-        tmp = json.load(get_post)
-
-    for p in tmp['posts']:
-        posts.append(p)
-
-        # data = json.dumps(p, ensure_ascii=False)
-        # req = urllib2.Request(url, data)
-        # req.add_header('Content-type', 'application/json')
-        # response = urllib2.urlopen(req)
-        # the_page = response.read()
-        # print(the_page)
+    # Get posts from 100 blogs
+    while len(links_to_blogs) != 0 and count < 100:
+        get_posts_from_blogs(links_to_blogs.pop(), count)
 
 if __name__ == '__main__':
     try:
